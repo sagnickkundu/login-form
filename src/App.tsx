6eae1b4e353e51318be43edfc921c328397f2cdc
Sagnick Kundu <sagnickkundu@Sagnicks-MacBook-Air.local>
2022-09-21 16:30:11 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    usernameRef.current!.focus();
+  }, []);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const username = usernameRef.current!.value;
+    const password = passwordRef.current!.value;
+    console.log("Username: "+ username + " Password: " + password);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>LOGIN</h3>
+      <div className="form">
+        <form onSubmit={submitHandler} className="login-form">
+          <input placeholder="Username" type="text" ref={usernameRef} />
+          <input placeholder="Password" type="password" ref={passwordRef} />
+          <div className="suffix">
+            <div className="remember-me">
+              <input id="rememberMe" type="checkbox" />
+              <label htmlFor="rememberMe">Remember me</label>
+            </div>
+            <a href="/#" className="forgot">Forgot?</a>
+          </div>
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
     </div>
   );
 }
